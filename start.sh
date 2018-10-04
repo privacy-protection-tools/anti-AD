e76c51d79274dc608e6280e489d1d64e753f003d
gently <gently@live.cn>
2018-10-04 21:37:00 +0800

    添加easylist，更换hosts源

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+source /etc/profile
+
+cd $(dirname $(readlink -f "$0"))
+echo '开始下载 easylist1...'
+wget -O easylistchina+easylist.txt https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt
+
+if [ $? -ne 0 ];then
+	echo '下载失败，请重试'
+	exit 1
+fi
+
+echo '开始下载 easylist2...'
+wget -O koolproxy.txt https://kprule.com/koolproxy.txt
+
+if [ $? -ne 0 ];then
+	echo '下载失败，请重试'
+	exit 1
+fi
+
+php make-addr.php
