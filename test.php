965b99f9745f324712c3a4e544bf6aea771addd7
gently <gently@live.cn>
2018-10-08 11:39:24 +0800

    delete something

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$a = array('uu'=>array('xx' => 1, 'yy' => 2),'pp' => 123, 'zz' => 0);
-$b = array('cc' => array('xx' => 78989, 'yy' => 99), 'uu' => array('zz' => 989), 'pp' => 65);
-
-var_dump(array_merge_recursive($a, $b));
