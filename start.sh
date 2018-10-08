093c78000a119f835c3a0a21478ebb54d791cc61
gentlyx <gently@live.cn>
2018-10-08 12:44:01 +0800

    auto commit

--- start.sh
+++ start.sh
@@ -21,4 +21,8 @@ if [ $? -ne 0 ];then
 	exit 1
 fi
 
-php make-addr.php
+/usr/local/php/bin/php make-addr.php
+
+git pull
+git commit -a -m "auto commit"
+git push https://gentlyx:3d5431cc8f2586effaee94efb7d39904db3a3a0c@github.com/gentlyx/anti-AD.git
