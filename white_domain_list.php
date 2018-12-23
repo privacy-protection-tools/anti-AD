e316749b25a590ac33a414738b90de669732a1d3
gently <gently@live.cn>
2018-12-23 17:12:58 +0800

    update
    update white list

--- white_domain_list.php
+++ white_domain_list.php
@@ -1,12 +1,13 @@
 <?php
 //white_domain_list
 //白名单机制...
-//@date 2018年11月17日
+//@date 2018年12月23日
 
 return array(
 
 	'cdn-thumb.fds.api.xiaomi.com'		=> true,
 	'.cdn-thumb.fds.api.xiaomi.com' 	=> true,
-	'bce.baidu.com'				=> true,
-	'.bce.baidu.com'			=> true,
+	'bce.baidu.com'						=> true,
+	'.bce.baidu.com'					=> true,
+	'b.bdstatic.com'					=> true,
 );
