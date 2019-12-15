8c51b9f645d37b730d6633fb01f001a3577a30d2
gently <gently@live.cn>
2019-12-15 17:56:26 +0800

    update

--- tools/start-valid.sh
+++ tools/start-valid.sh
@@ -5,12 +5,11 @@ source /etc/profile
 cd $(cd "$(dirname "$0")";pwd)
 
 cd ../origin-files
-
 rm -f split-tmp-list_*
-
 split -l 5000 ../adblock-for-dnsmasq.conf split-tmp-list_
-
-cd ../tools
+cd ../
+git pull
+cd tools
 
 # shellcheck disable=SC2045
 for f in $(ls ../origin-files/split-tmp-list_*)
