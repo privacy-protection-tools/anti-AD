61d0539b481ad71eb020889b0d09f4f4b6470494
gently <gently@live.cn>
2019-12-17 20:35:43 +0800

    bug fix

--- lib/addressMaker.class.php
+++ lib/addressMaker.class.php
@@ -74,7 +74,7 @@ class addressMaker {
                 continue;
             }
 
-            if(preg_match('/^\|\|([0-9a-z\-\.]+[a-z]+)[\^\$]*(image|third-party|script)?(,.+)?$/', $line, $matches)){
+            if(preg_match('/^\|\|([0-9a-z\-\.]+[a-z]+)[\^\$]*([\~0-9a-zA-Z\.\-_]+,)?(image|third-party|script)(,.+)?$/', $line, $matches)){
 
                 if(substr($matches[1], 0, 4) == 'www.'){
                     $row = substr($matches[1], 4);
