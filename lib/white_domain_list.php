a617a72954e2545879f756934e4dba0205b836d5
gently <gently@live.cn>
2019-12-18 14:57:12 +0800

    update white domain list

--- lib/white_domain_list.php
+++ lib/white_domain_list.php
@@ -11,7 +11,10 @@ return array(
 	'b.bdstatic.com'					=> true,
 	'googleadapis.l.google.com'			=> true, //解决google字体下载异常
 	'gstaticadssl.l.google.com'			=> true, //解决google字体下载异常
+	'gvt10.com'							=> true,
 	'gvt1.com'							=> true,
+	'gvt2.com'							=> true,
+	'gvt3.com'							=> true,
 	'wangbase.com'						=> true,
 	'l.qq.com'							=> true, //解决腾讯视频无法播放
 	'g.csdnimg.cn'						=> true, //csdn阅读更多
