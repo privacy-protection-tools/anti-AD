8e2a8facfa6de5885959555cfd18ef34c4bb1cbc
gently <gently@live.cn>
2019-12-29 11:09:58 +0800

    update research shell script

--- tools/start-research.sh
+++ tools/start-research.sh
@@ -6,8 +6,8 @@ cd $(cd "$(dirname "$0")";pwd)
 
 nohup php research-addr.php >> ./std-research.out
 cd ../dist/
-sed -E 's/address=\/(.+)?\//||\1^/g' anti-ad-dnsmasq-full.conf > anti-ad-easylist-full.conf
-sed -E 's/address=\/(.+)?\//||\1^/g' anti-ad-dnsmasq-basic.conf > anti-ad-easylist-basic.conf
+sed -E 's/address=\/(.+)?\//||\1^/g' anti-ad-dnsmasq-full.conf > anti-ad-easylist-full.txt
+sed -E 's/address=\/(.+)?\//||\1^/g' anti-ad-dnsmasq-basic.conf > anti-ad-easylist-basic.txt
 sed -E 's/address=\/(.+)?\//DOMAIN-SUFFIX,\1,REJECT/g' anti-ad-dnsmasq-basic.conf >anti-ad-surge-basic.txt
 sed -E 's/address=\/(.+)?\//DOMAIN-SUFFIX,\1,REJECT/g' anti-ad-dnsmasq-full.conf >anti-ad-surge-full.txt
 sed -i '3 i [RULE]' anti-ad-surge-basic.txt
@@ -15,4 +15,6 @@ sed -i '3 i [RULE]' anti-ad-surge-full.txt
 
 cd ../tools
 
+php easylist-extend.php dist/anti-ad-easylist-basic.txt
+php easylist-extend.php dist/anti-ad-easylist-full.txt
 rm -f std-split-tmp-list_*.out
