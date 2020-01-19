d52992bb3047627f603629f0828075a36b6e66d6
gently <1243610+gentlyxu@users.noreply.github.com>
2020-01-19 16:42:00 +0800

    fixed result string

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ git pull
 
 shopt -s expand_aliases
 alias php='/usr/local/php/bin/php'
-. ./start-ci.sh
+export PHP_RET=$(. ./start-ci.sh)
 
 git add -A adblock-for-dnsmasq.conf origin-files/*
 git commit -am "auto commit. script output--- $PHP_RET"
