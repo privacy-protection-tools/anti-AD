92b9852c97b5297691c4db1b35a7631d6e8c4eae
gently <gently@live.cn>
2020-04-11 21:46:49 +0800

    白名单更新

--- tools/easylist-extend.php
+++ tools/easylist-extend.php
@@ -108,6 +108,7 @@ $ARR_WHITE_RULE_LIST = array(
     '@@||widget.weibo.com^' => 0, //微博外链
     '@@||uland.taobao.com^' => 1, //淘宝coupon #83
     '@@||advertisement.taobao.com^' => 1, //CNAME 被杀，导致s.click.taobao.com等服务异常
+    '@@||baozhang.baidu.com^' => 1, //CNAME e.shifen.com 
 );
 
 //针对上游赦免规则anti-AD不予赦免的规则，即赦免名单的黑名单
