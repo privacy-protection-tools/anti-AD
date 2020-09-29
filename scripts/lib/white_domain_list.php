53d534fbe010d65c652857c698fedbac6027ee12
gently <1243610+gentlyxu@users.noreply.github.com>
2020-09-30 07:52:27 +0800

    微信滑动验证码页面修复

--- scripts/lib/white_domain_list.php
+++ scripts/lib/white_domain_list.php
@@ -160,6 +160,7 @@ return array(
     'iwx.mail.qq.com' => 0, // #243
     'ename.net' => 0, // #246
     'api.iplay.163.com' => 0, // #247
+    'minorshort.weixin.qq.com' => 0, // 微信滑动验证码 
     
     
     
