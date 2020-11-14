9fa8591f4fe6b4e79b047a6406aa18f61a44f4d6
gently <1243610+gentlyxu@users.noreply.github.com>
2020-11-14 17:03:05 +0800

    update. fixed #280

--- scripts/tools/easylist-extend.php
+++ scripts/tools/easylist-extend.php
@@ -193,6 +193,7 @@ $ARR_WHITE_RULE_LIST = array(
     '@@||track.cpau.info^' => 1, // #251
     '@@||passport.bobo.com^' => 1, // #265
     '@@||stat.jseea.cn^' => 1, // #279
+    '@@||widget.intercom.io^' => 1, // #280
 );
 
 //针对上游赦免规则anti-AD不予赦免的规则，即赦免名单的黑名单
