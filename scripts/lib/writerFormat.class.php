90c346e466dc57d4fbfdb214cab21f1262d7a101
Ariesly <Ariesly.Mao@hotmail.com>
2020-11-19 19:29:39 +0800

    Add Calsh 列表 - anti-ad-clash.yaml

--- scripts/lib/writerFormat.class.php
+++ scripts/lib/writerFormat.class.php
@@ -64,6 +64,34 @@ class writerFormat{
         ),
     );
 
+    /*Clash 格式的屏蔽广告列表*/
+    const CLASH = array(
+        'format' => '  - DOMAIN-SUFFIX,{DOMAIN}',
+        'header' => "#TITLE=anti-AD\n#VER={DATE}\n#URL={URL}\n#TOTAL_LINES={COUNT}\npayload:\n",
+        'full_domain' => 0,
+        'name' => 'clash',
+        'filename' => '../anti-ad-clash.yaml',
+        'whitelist_attached' => array(
+            'base-dead-hosts.txt' =>array(
+                'merge_mode' => 2, //0=单条，1=单条+子域名，2=根域名相当于1，非根域名相当于0
+            ),
+        ),
+        'src' => array(
+            'base-src-easylist.txt' => array(
+                'type' => 'easylist',
+                'strict_mode' => false,
+            ),
+            'base-src-hosts.txt' => array(
+                'type' => 'hosts',
+                'strict_mode' => false,
+            ),
+            'base-src-strict-hosts.txt' => array(
+                'type' => 'hosts',
+                'strict_mode' => true,
+            ),
+        ),
+    );
+
     /*Surge 兼容格式的屏蔽广告列表*/
     const SURGE = array(
         'format' => 'DOMAIN-SUFFIX,{DOMAIN}',
