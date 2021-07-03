33638386641bd95d5fd5fc8e081d66de442ec24f
gently <1243610+gentlyxu@users.noreply.github.com>
2021-07-03 22:58:40 +0800

    fixed adguard list's ver number

--- scripts/build-list.sh
+++ scripts/build-list.sh
@@ -7,7 +7,7 @@ cd $(cd "$(dirname "$0")";pwd)
 php make-addr.php
 echo
 echo "!Title: anti-AD for AdGuard" > ../anti-ad-adguard.txt
-echo "!Version: 20210530181117" >> ../anti-ad-adguard.txt
+echo "!Version: $(date "+%Y%m%d%H%M%S%N")" >> ../anti-ad-adguard.txt
 echo "!Homepage: https://anti-ad.net/" >> ../anti-ad-adguard.txt
 echo "!!!note! If you are using \"AdGuard Home\", please load https://anti-ad.net/easylist.txt" >> ../anti-ad-adguard.txt
 echo "!Total lines: 00000" >> ../anti-ad-adguard.txt
