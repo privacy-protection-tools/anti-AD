cb2368080d351dcfe860fdcd5120e4f467939de1
gently <1243610+gentlyxu@users.noreply.github.com>
2021-07-03 16:15:04 +0800

    update. fixed #456

--- scripts/tools/easylist-extend.php
+++ scripts/tools/easylist-extend.php
@@ -138,7 +138,7 @@ $ARR_MERGED_WILD_LIST = array(
 
 $ARR_REGEX_LIST = array(
     '/^(\S+\.)?9377[a-z0-9]{2}\.com$/' => ['m' => '$dnstype=A'],
-    '/^(\S+\.)?ad(s?[\d]+|m|s)?\./' => null,
+    '/^(\S+\.)?ad(s?[\d]+|m|s)?\./' => ['m' => '$denyallow=nucdn.net|azureedge.net'],
     '/^(\S+\.)?advert/' => ['m' => '$denyallow=alibabacorp.com|alibabadns.com|sm.cn|tanx.com|alibaba-inc.com|tmall.com|taobao.com'],
     '/^(\S+\.)?affiliat(es?[0-9a-z]*?|ion[0-9\-a-z]*?|ly[0-9a-z\-]*?)\./' => null, // fixed #406
     '/^(\S+\.)?s?metrics\./' => null, // TODO 覆盖面很大
