fd55558529ae3696d191751ad22b65b74da0e0e2
gently <1243610+gentlyxu@users.noreply.github.com>
2022-10-16 22:10:21 +0800

    fixed #714

--- scripts/tools/easylist-extend.php
+++ scripts/tools/easylist-extend.php
@@ -259,6 +259,7 @@ $ARR_WHITE_RULE_LIST = array(
     '@@||adashx.ut.dingtalk.com^' => 1, // #662
     '@@||h-adashx.ut.dingtalk.com^' => 1, // #662
     '@@||ads.95516.com^' => 1, // #695
+    '@@||track.bankcomm.com^' => 1, // #714
 );
 
 //针对上游赦免规则anti-AD不予赦免的规则，即赦免名单的黑名单
