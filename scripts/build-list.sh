fd4d12fa28acc063c5ff6db304f34eeea4e07341
gently <1243610+gentlyxu@users.noreply.github.com>
2023-01-07 14:56:36 +0800

    update. #747

--- scripts/build-list.sh
+++ scripts/build-list.sh
@@ -8,6 +8,7 @@ php make-addr.php
 echo
 echo "!Title: anti-AD for AdGuard" > ../anti-ad-adguard.txt
 echo "!Version: $(date "+%Y%m%d%H%M%S%N")" >> ../anti-ad-adguard.txt
+echo "!Expires: 4 days" >> ../anti-ad-adguard.txt
 echo "!Homepage: https://anti-ad.net/" >> ../anti-ad-adguard.txt
 echo "!!!note! If you are using \"AdGuard Home\", please load https://anti-ad.net/easylist.txt" >> ../anti-ad-adguard.txt
 echo "!Total lines: 00000" >> ../anti-ad-adguard.txt
