6b7636a768b716c8802669dae0fd7a54329c93b2
gently <1243610+gentlyxu@users.noreply.github.com>
2023-04-30 00:17:50 +0800

    fixed #787

--- scripts/lib/white_domain_list.php
+++ scripts/lib/white_domain_list.php
@@ -397,6 +397,7 @@ return [
     "www.mairuan.cn" => 0, #772
     "mairuan.net" => 0, #772
     "www.mairuan.net" => 0, #772
+    "ldmnq.com" => 0, #787
     "iflow.uczzd.cn" => 0, // uc搞笑段子
     "m.uczzd.cn" => 0, // uc搞笑段子
     "puds.ucweb.com" => 0, // uc小游戏
