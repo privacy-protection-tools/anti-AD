b922707a36b6a9da1a9d31a94a56275b640ab685
gently <1243610+gentlyxu@users.noreply.github.com>
2023-06-01 21:52:41 +0800

    fixed #797

--- scripts/lib/white_domain_list.php
+++ scripts/lib/white_domain_list.php
@@ -400,6 +400,7 @@ return [
     "ldmnq.com" => 0, #787
     "heytapimage.com" => 0, #779, #780
     "fexclick.baidu.com" => 0, #790
+    "jango.com" => 0, #797
     "iflow.uczzd.cn" => 0, // uc搞笑段子
     "m.uczzd.cn" => 0, // uc搞笑段子
     "puds.ucweb.com" => 0, // uc小游戏
