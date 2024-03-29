4434c62f5fa22d12f1d1278245046d7dcd9294ce
gentlyxu <1243610+gentlyxu@users.noreply.github.com>
2024-03-30 00:59:29 +0800

    #907

--- scripts/lib/white_domain_list.php
+++ scripts/lib/white_domain_list.php
@@ -424,6 +424,7 @@ return [
     "vip.cainiaofx.com" => 0, // #904
     "ib.snssdk.com" => 0, // #905
     "audid-api.taobao.com" => 0, // #906
+    "m.intl.taobao.com" => 0, // #907
     "iflow.uczzd.cn" => 0, // uc搞笑段子
     "m.uczzd.cn" => 0, // uc搞笑段子
     "puds.ucweb.com" => 0, // uc小游戏
