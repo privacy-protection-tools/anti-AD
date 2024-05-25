4c71c2336681dbc9afd05e066a378b4e3f578d0f
gentlyxu <1243610+gentlyxu@users.noreply.github.com>
2024-05-25 09:14:14 +0800

    fixed #920

--- scripts/lib/white_domain_list.php
+++ scripts/lib/white_domain_list.php
@@ -429,6 +429,8 @@ return [
     "ads.console.aliyun.com" => 0, // #912
     "mcdn.bilivideo.cn" => 0, // #918
     "deliver.kuwo.cn" => 0, // #917
+    "log.snssdk.com" => 0, // #920
+    "i.snssdk.com" => 0, // #920
     "iflow.uczzd.cn" => 0, // uc搞笑段子
     "m.uczzd.cn" => 0, // uc搞笑段子
     "puds.ucweb.com" => 0, // uc小游戏
