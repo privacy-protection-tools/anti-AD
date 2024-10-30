44d1e56f8806eb1f7d5d932ddb85b6008569b006
MkQtS <81752398+MkQtS@users.noreply.github.com>
2024-10-30 15:42:58 +0800

    fix #974

--- scripts/lib/white_domain_list.php
+++ scripts/lib/white_domain_list.php
@@ -442,6 +442,7 @@ return [
     "tracker.ai.xiaomi.com" => 0, // #960
     "awstrack.me" => 0, // #964
     "c.paypal.com" => 0, // #965
+    "cc.map.qq.com" => 0, // #974
 
     "iflow.uczzd.cn" => 0, // uc搞笑段子
     "m.uczzd.cn" => 0, // uc搞笑段子
