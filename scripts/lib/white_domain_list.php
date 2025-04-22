3926184b300b6d7fa7253ec9699fd57e96109a4f
MkQtS <81752398+MkQtS@users.noreply.github.com>
2025-04-22 13:30:50 +0800

    fix #1033

--- scripts/lib/white_domain_list.php
+++ scripts/lib/white_domain_list.php
@@ -446,6 +446,7 @@ return [
     "cc.map.qq.com" => 0, // #974
     "xp.apple.com" => 0, // #991, #972
     "pkoplink.com" => 0, // #998
+    "r.stripe.com" => 0, // #1033
 
     "iflow.uczzd.cn" => 0, // uc搞笑段子
     "m.uczzd.cn" => 0, // uc搞笑段子
